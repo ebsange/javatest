66b16b7f861cc55e66ea1161a1e37cd60a073d77
ebsange <chengye@163.net>
2026-02-07 02:48:26 +0000

    git commit -m ""

--- Puppy.java
+++ Puppy.java
@@ -1,47 +1,31 @@
 public class Puppy {
-    //variable
+    //instance variable
     private int age;
     private String name;
-    private static String sex = "male";
+    private String color;
+    private static String status = "live";
  
     // 构造器constrctor with parameter
-    public Puppy(String name, int age) {
-        this.name = name;
-        this.age = age;
-    }
-    //none-argument constructor
-    public Puppy() {
-        name = "AAA";
-        age = 5;
-    }
-    //public return-type(数据类型，void) name
-    public void setAge(int age) {
-        this.age += age;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-    //overload
-    public int addAge(int newAge) {
+    public Puppy(String newName, int newAge, String newColor) {
+        name = newName;
         age = newAge;
+        color = newColor;
+    }
+
+
+    //overload
+    public void addAge(int newAge) {
+        age += newAge;
+    }
+
+    public double addAge(double newAge) {
+        age += newAge;
         return age;
     }
- 
-    public String addAge(String newName) {
-        return name;
-    }
-    public int doubleage() {
 
-        return age+=2;
-    }
-
-    public int getAge(){
+    public int getAge() {
         return age;
     }
 
-    public static String getSex(){
-        return sex;
-    }
 
 }
--- PuppyTest.java
+++ PuppyTest.java
@@ -1,14 +1,10 @@
 public class PuppyTest {
-    // 主方法
+    // client class
     public static void main(String[] args) {
-       Puppy a = new Puppy("a",4);
-       Puppy b = new Puppy();
-       Puppy c = a;
-       a.setName("bb");
-       System.out.println(a.addAge("ccc")));
-       System.out.println(c.addAge(4));
-       System.out.println(a.getAge());
-       System.out.println(Puppy.getSex());
-       //System.out.println(b.doubleage());
+        //初始化object
+       Puppy a = new Puppy("a", 4, "black");
+       Puppy b = a;
+       a.addAge(5);
+       System.out.println(b.getAge());
     }
 }
